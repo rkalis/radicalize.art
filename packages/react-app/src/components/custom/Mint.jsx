371674ae1b64397a9af2418ca762724aa466c7ed
StefanVelkoski <velkoskis@gmail.com>
2021-03-21 14:29:07 +0100

    mint front

--- packages/react-app/src/components/custom/Mint.jsx
+++ packages/react-app/src/components/custom/Mint.jsx
@@ -1,28 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Main.css"
 
 export default function Mint() {
+
+  const initialFormData = Object.freeze({
+    file: "",
+    name: "",
+    description: "",
+    newPrice: "",
+    patronageRate: "",
+    checked: false
+  })
+
+  const [formData, updateFormData] = useState(initialFormData);
+
+
+  const handleChange = (e) => {
+    updateFormData({
+      ...formData,
+
+      [e.target.name]: e.target.value
+
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData);
+  };
+
     return (
+
      <div className="row">
        <div className="col-md-4"></div>
 
        <div className="col-md-4">
 
-        <form className="form-align">
+        <form className="form-align" action="">
         <div className="title" >MINT</div>
         <hr/>
 
           <div className="form-group">
-            <input type="file" className="form-control-file" id="file" name="file" />
+            <input type="file" className="form-control-file" id="file" name="file" required onChange={handleChange}/>
           </div>
 
           <div class="form-group">
-              <textarea className="form-control" id="name" name="name" rows="2" placeholder="Name your NFT...">
+              <textarea className="form-control" id="name" name="name" rows="2" placeholder="Name your NFT..."  required onChange={handleChange}>
               </textarea>
           </div>
 
           <div class="form-group">
-              <textarea className="form-control" id="description" name="description" rows="6" placeholder="Describe your NFT.."></textarea>
+              <textarea className="form-control" id="description" name="description" rows="6" placeholder="Describe your NFT.." required onChange={handleChange}></textarea>
           </div>
 
           <div className="title" >RADICALIZE</div>
@@ -31,12 +59,12 @@ export default function Mint() {
           <div className="row">
             <div className="col-md-6">
               <div class="form-group textarea-radical">
-                <textarea className="form-control" id="newprice" name="price"rows="3" placeholder="New price..."></textarea>
+                <textarea className="form-control" id="newprice" name="newPrice" rows="3" placeholder="New price..." required onChange={handleChange}></textarea>
               </div>
             </div>
             <div className="col-md-6">
               <div class="form-group textarea-radical ">
-                <textarea className="form-control" id="patronageprice"  name="patronagerate" placeholder="Patronage rate..." rows="3"></textarea>
+                <textarea className="form-control" id="patronageprice"  name="patronageRate" placeholder="Patronage rate..." rows="3" required onChange={handleChange}></textarea>
               </div>
             </div>
 
@@ -44,16 +72,16 @@ export default function Mint() {
           </div>
 
           <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="checkbox" name="checkbox"/>
+          <input class="form-check-input" type="checkbox" value="" id="checkbox" name="checked" required onChange={handleChange} readOnly/>
           <label class="form-check-label" for="checkbox">
-          Use decreasing price function for price discovery mechanism
+              Use decreasing price function for price discovery mechanism
           </label>
           </div>
 
           <hr/>
 
           <p className="note"> NOTE: WHILE YOU KEEP THE RADICAL TOKEN, YOU WILL NOT HAVE TO PAY THE PATRONAGE</p>
-          <button className="btn btn-primary">CREATE NFT</button>
+          <button className="btn btn-primary" type="submit" onClick={handleSubmit}>CREATE NFT</button>
 
           </form>
        </div>
